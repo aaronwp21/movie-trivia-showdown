621d7b711b0fb6cce8324a4cd5f79267cdaa6103
aaronwp21 <110860467+aaronwp21@users.noreply.github.com>
2023-09-19 12:23:51 +0100

    added spin again text and keep button

--- src/components/QuestionPicker.tsx
+++ src/components/QuestionPicker.tsx
@@ -17,10 +17,16 @@ const categories = [
 ];
 
 function QuestionPicker() {
+  const [spinNum, setSpinNum] = useState(0);
   const [selectedNum, setSelectedNum] = useState(-1);
 
   const spin = () => {
-    setSelectedNum(questionPickerRandomNumber())
+    setSpinNum(spinNum + 1);
+    const num = questionPickerRandomNumber();
+    setSelectedNum(num);
+    if (spinNum === 0) {
+
+    }
   }
 
   return (
@@ -28,15 +34,21 @@ function QuestionPicker() {
       <ul className="mb-4 grid grid-cols-[repeat(auto-fit,minmax(150px,1fr))] gap-4 text-sm">
         {categories.map((category, i) => {
           return (
-            <li key={i} className={`${selectedNum === i ? 'bg-white text-primary' : 'bg-black'} px-2 py-4 text-center`}>
+            <li key={i} className={`${selectedNum === i ? 'bg-white text-primary font-bold' : 'bg-black'} px-2 py-4 text-center`}>
               {category}
             </li>
           );
         })}
       </ul>
-      <button onClick={() => spin()} className="bg-white text-primary font-bold border-2 border-white py-2 px-16 rounded-lg hover:border-white hover:bg-primary hover:text-white self-center">
-        Spin
-      </button>
+      <p className={`${spinNum === 0 ? 'hidden' : 'block'} text-center text-lg font-semibold mb-4`}>Would You Like to Spin Again?</p>
+      <div className='flex justify-center gap-4'>
+        <button onClick={() => spin()} className="bg-white text-primary font-bold border-2 border-white py-2 px-16 rounded-lg hover:border-white hover:bg-primary hover:text-white">
+          Spin
+        </button>
+        <button className={`${spinNum === 0 ? 'hidden' : 'block'} bg-black text-white font-bold py-2 px-16 rounded-lg`}>
+          Keep
+        </button>
+      </div>
     </>
   );
 }
