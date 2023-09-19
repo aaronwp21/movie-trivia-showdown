2fe90f0989185ef2988b1e9c55bb701cee6cfb87
aaronwp21 <110860467+aaronwp21@users.noreply.github.com>
2023-09-19 12:30:29 +0100

    removed button and text on spin again

--- src/components/QuestionPicker.tsx
+++ src/components/QuestionPicker.tsx
@@ -25,27 +25,48 @@ function QuestionPicker() {
     const num = questionPickerRandomNumber();
     setSelectedNum(num);
     if (spinNum === 0) {
-
     }
-  }
+  };
 
   return (
     <>
-      <ul className="mb-4 grid grid-cols-[repeat(auto-fit,minmax(150px,1fr))] gap-4 text-sm">
+      <ul className="mb-8 grid grid-cols-[repeat(auto-fit,minmax(150px,1fr))] gap-4 text-sm">
         {categories.map((category, i) => {
           return (
-            <li key={i} className={`${selectedNum === i ? 'bg-white text-primary font-bold' : 'bg-black'} px-2 py-4 text-center`}>
+            <li
+              key={i}
+              className={`${
+                selectedNum === i
+                  ? 'bg-white text-primary font-bold'
+                  : 'bg-black'
+              } px-2 py-4 text-center`}
+            >
               {category}
             </li>
           );
         })}
       </ul>
-      <p className={`${spinNum === 0 ? 'hidden' : 'block'} text-center text-lg font-semibold mb-4`}>Would You Like to Spin Again?</p>
-      <div className='flex justify-center gap-4'>
-        <button onClick={() => spin()} className="bg-white text-primary font-bold border-2 border-white py-2 px-16 rounded-lg hover:border-white hover:bg-primary hover:text-white">
+      <p
+        className={`${
+          spinNum === 0 || spinNum === 2 ? 'hidden' : 'block'
+        } text-center text-lg font-semibold mb-4`}
+      >
+        Would You Like to Spin Again?
+      </p>
+      <div className="flex justify-center gap-4">
+        <button
+          onClick={() => spin()}
+          className={`${
+            spinNum === 2 ? 'hidden' : 'block'
+          } bg-white text-primary font-bold border-2 border-white py-2 px-16 rounded-lg hover:border-white hover:bg-primary hover:text-white`}
+        >
           Spin
         </button>
-        <button className={`${spinNum === 0 ? 'hidden' : 'block'} bg-black text-white font-bold py-2 px-16 rounded-lg`}>
+        <button
+          className={`${
+            spinNum === 0 ? 'hidden' : 'block'
+          } bg-black text-white font-bold py-2 px-16 rounded-lg`}
+        >
           Keep
         </button>
       </div>
