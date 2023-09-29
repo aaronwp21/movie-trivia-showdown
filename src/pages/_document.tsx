78e43ef8b23772fdcc2b71cc7dce640d24e59ecd
aaronwp21 <110860467+aaronwp21@users.noreply.github.com>
2023-09-30 00:42:29 +0100

    added favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,8 +10,29 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&family=Righteous&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ea0006" />
+        <meta name="msapplication-TileColor" content="#b91d47" />
+        <meta name="theme-color" content="#ffffff"></meta>
       </Head>
-      <body className='min-w-[400px] bg-primary'>
+      <body className="min-w-[400px] bg-primary">
         <Main />
         <NextScript />
       </body>
